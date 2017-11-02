9385a0640dae161e41f5b56f6fe3a487297af648
Johnnie Regalado <johnniereg@gmail.com>
2017-11-02 21:30:20 +0000

    Stretch problem solved.

--- closures-stretch.js
+++ closures-stretch.js
@@ -1,10 +1,16 @@
 var wrapLog = function (callback, name) {
-  /* your code here */
+  return function () {
+    var args = Array.from(arguments).join(", ");
+    var results = callback.apply(null, arguments);
+    var string = name + "(" + args + ") => " + results;
+    console.log(string);
+  };
 };
 
 var area = function (x, y) {
   return x * y;
 };
+
 var logArea = wrapLog(area, "area");
 
 logArea(5, 3); // area(5, 3) => 15
@@ -13,6 +19,7 @@ logArea(3, 2); // area(3, 2) => 6
 var volume = function (x, y, z) {
   return x * y * z;
 };
+
 var logVolume = wrapLog(volume, "volume");
 
 logVolume(5, 3, 2); // volume(5, 3, 2) => 30
